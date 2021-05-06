8e024dfbbc83d07a5b769a7b0d1b0f5badb3e6df
WalterKstro <walterkstro@gmail.com>
2021-05-05 22:58:38 -0600

    Testing the component <CategoryItem/>

--- src/tests/components/CategoryItem.test.js
+++ src/tests/components/CategoryItem.test.js
@@ -2,14 +2,36 @@ import CategoryItem from '../../components/Category/CategoryItem'
 import { shallow } from "enzyme";
 
 describe('tests in componente <CategoryItem/>', () => {
+    
+    const title = 'naruto shippuden GIF'
+    const url = 'https://lorempixel.com'
+    const renderedComponent = shallow(<CategoryItem url={url} title={title}/>)
 
     test('should show correctly the component <CategoryItem/>',() => {
-        const title = 'naruto shippuden GIF'
-        const url = 'https://media2.giphy.com/media/4gsjHZMPXdlGo/giphy.gif?cid=78531bbftw97gr2ypzgv1ae9i7nds2onndui22hxil9rj5wt&rid=giphy.gif&ct=g'
-
-        const renderedComponent = shallow(<CategoryItem url={url} title={title}/>)
-
         expect(renderedComponent).toMatchSnapshot()
     })
+
+
+    test('should show the tag figcaption with the prop title', () => {
+        const findTitle = renderedComponent.find('figcaption').text()
+        expect(findTitle).toBe(title)
+    })
+
+    test('should have atributes src and alt with the props', () => {
+        const {src, alt} = renderedComponent.find('img').props()
+        
+        expect(src).toBe(url)
+        expect(alt).toBe(title)
+
+    })
+
+
+    test('should have the class animate__fadeInLeft', () => {
+        const hasClass = renderedComponent.find('div').hasClass('animate__fadeInLeft')
+        expect(hasClass).toBe(true)
+    })
+    
+    
+    
 
 })
