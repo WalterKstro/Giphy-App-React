c159e3d818444213ebe41917cc1053d7af31a2fb
WalterKstro <walterkstro@gmail.com>
2021-05-17 22:40:43 -0600

    Testing the number of components <CategoryItem is equals to the elements of the data

--- src/tests/components/CategoryGrid.test.js
+++ src/tests/components/CategoryGrid.test.js
@@ -18,15 +18,22 @@ describe('Testing in the component <CategoryGrid/>', () => {
     })
 
 
-    test('should show item with the data', () => {
-        const gifts = [{id:'_se56qs23',url: 'www.facebook.com',title:'Naruto'}]
+    test('should show the component <CategoryItem/> if exist data and no should show the tag <p> if data exist', () => {
+        const gifts = [
+            {id:'_se56qs23',url: 'www.facebook.com',title:'Naruto'},
+            {id:'_se5qqs03',url: 'www.facebook.com',title:'One Punch Man'}
+        ]
 
         useFetch.mockReturnValue({
             data : gifts,
             loading : false
         })
         const component = shallow( <CategoryGrid category={category}/> )
-        expect( component).toMatchSnapshot()
+        const paraghrap = component.find('.loadding')
+        
+        expect(component).toMatchSnapshot()
+        expect(paraghrap.exists()).not.toBe(true)
+        expect(component.find('CategoryItem').length).toBe(gifts.length)
     })
     
 
